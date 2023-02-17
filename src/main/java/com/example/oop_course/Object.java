09734df43d9d6481edde55dcef398c2535c728d4
Gleb Pimenov <gleb.pimenov7@gmail.com>
2023-02-17 14:59:24 +0300

    Классы изменены в соотвествии с MVC

--- src/main/java/com/example/oop_course/Object.java
+++ src/main/java/com/example/oop_course/Object.java
@@ -2,18 +2,8 @@ package com.example.oop_course;
 
 public abstract class Object {
     int id;
-    static int idCounter;
-
-    private boolean setId(int id) {
-        this.id = id;
-        return true;
-    }
 
     public int getId() {
         return id;
-    }
-
-    public static int getIdCounter() {
-        return idCounter;
     }
 }
--- src/main/java/com/example/oop_course/Route.java
+++ src/main/java/com/example/oop_course/Route.java
@@ -1,6 +1,5 @@
 package com.example.oop_course;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public final class Route extends Object {
@@ -8,66 +7,17 @@ public final class Route extends Object {
     private List<Bus> buses;
     private int number;
     private int start;
-    private static List<Route> routes;
-
-    public boolean addBus(Bus bus) {
-        return false;
-    }
-
-    public boolean addPoint(Point point) {
-        return false;
-    }
 
     public int getNumber() {
         return number;
     }
 
-    public boolean setNumber(int number) {
-        this.number = number;
-        return true;
-    }
-
     public List<Bus> getBuses() {
-        return new ArrayList<>();
-    }
-
-    private boolean setBuses(List<Bus> buses) {
-        this.buses = buses;
-        return true;
+        return buses;
     }
 
     public List<Point> getPoints() {
-        return new ArrayList<>();
-    }
-
-    private boolean setPoints(List<Point> points) {
-        this.points = points;
-        return true;
-    }
-
-    public boolean removePoint(Point point) {
-        return false;
-    }
-
-    public boolean removeBus(Bus bus) {
-        return false;
-    }
-
-    private boolean addRoute(Route route) {
-        return false;
-    }
-
-    private boolean removeRoute(Route route) {
-        return false;
-    }
-
-    public static List<Route> getRoutes() {
-        return new ArrayList<>();
-    }
-
-    public boolean setStart(int start) {
-        this.start = start;
-        return true;
+        return points;
     }
 
     public int getStart() {
